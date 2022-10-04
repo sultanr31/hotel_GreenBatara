f97fcd84bc2d7362a52acd5b29aa5b98ad191467
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-10-04 22:53:04 +0700

    Update

---
+++ app/Views/index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.6.8, mobirise.com">
+  <meta name="twitter:card" content="summary_large_image"/>
+  <meta name="twitter:image:src" content="">
+  <meta property="og:image" content="">
+  <meta name="twitter:title" content="Beranda">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="CH.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>CH Hotel</title>
+  <link rel="stylesheet" href="/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="/dropdown/css/style.css">
+  <link rel="stylesheet" href="/socicon/css/styles.css">
+  <link rel="stylesheet" href="/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+  
+</head>
+<body>
+  
+  <section data-bs-version="5.1" class="menu cid-s48OLK6784" once="menu" id="menu1-h">
+    
+    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+        <div class="container">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a>
+                        <img src="https://media.discordapp.net/attachments/768689302010986546/1026721760613244928/GreemBatara.png?width=480&height=480" alt="Mobirise Website Builder" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white text-primary display-7" href="home.php">Green Batara Hotel</a></span>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+                    <li class="nav-item"><a class="nav-link link text-white display-4" href="rservice.php">Room Service</a></li>
+                    <li class="nav-item"><a class="nav-link link text-white display-4" href="checkin.php">My Order</a></li>
+                    <li class="nav-item"><a class="nav-link link text-white display-4" href="customer.php">Admin</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</section>
+
+<section data-bs-version="5.1" class="features16 cid-t6E7AIUsEH" id="features17-t">
+    <div class="container">
+        <div class="content-wrapper">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-6">
+                    <div class="image-wrapper">
+                        <img src="/images/view-1160x599.jpg" alt="Mobirise Website Builder">
+                    </div>
+                </div>
+                <div class="col-12 col-lg">
+                    <div class="text-wrapper">
+                        <h6 class="card-title mbr-fonts-style display-5">
+                            <strong>Selamat Datang!</strong></h6>
+                        <p class="mbr-text mbr-fonts-style mb-4 display-4">
+                        Terinspirasi oleh keindahan Green Batara, Hotel Green Batara Bandung menawarkan pemandangan gunung yang spektakuler, santapan yang memanjakan, kamar-kamar yang elegan, dan keramahan luar biasa yang diwujudkan oleh Butler Service 24 jam. Terletak di perbukitan Ciumbuleuit, namun hanya beberapa menit dari hiruk pikuk pusat kota Bandung.</p>
+                        <div class="mbr-section-btn mt-3"><a class="btn btn-black display-7" href="reservasi.php"><span class="mobi-mbri mobi-mbri-plus mbr-iconfont mbr-iconfont-btn" style="font-size: 20px;"></span>Pesan Kamar</a>
+                        <a class="btn btn-success display-7" href="customer.php"><span class="mobi-mbri mbr-iconfont mbr-iconfont-btn" style="font-size: 17px;"></span>Halaman Admin</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="gallery5 mbr-gallery cid-t6E75YveNg" id="gallery5-q">
+    
+
+    
+    
+
+    <div class="container">
+        <div class="mbr-section-head">
+            <h3 class="mbr-section-title mbr-fonts-style align-center m-0 display-2"><strong>Tipe Kamar</strong></h3>
+            <h4 class="mbr-section-subtitle mbr-fonts-style align-center mb-0 mt-2 display-7">Silahkan pesan kamar di halaman beranda.</h4>
+        </div>
+        <div class="row mbr-gallery mt-4">
+            <div class="col-12 col-md-6 col-lg-3 item gallery-image">
+                <div class="item-wrapper" data-toggle="modal" data-bs-toggle="modal" data-target="#t6Em4xNi0C-modal" data-bs-target="#t6Em4xNi0C-modal">
+                    <img class="w-100" src="/images/2-596x447.jpg" alt="Mobirise Website Builder" data-slide-to="0" data-bs-slide-to="0" data-target="#lb-t6Em4xNi0C" data-bs-target="#lb-t6Em4xNi0C">
+                    <div class="icon-wrapper">
+                        <span class="mobi-mbri mobi-mbri-search mbr-iconfont mbr-iconfont-btn"></span>
+                    </div>
+                </div>
+                <h6 class="mbr-item-subtitle mbr-fonts-style align-center mb-2 mt-2 display-7">Studio 1</h6>
+                <h5 class="mbr-item-subtitle mbr-fonts-style align-center mb-2 mt-2 display-7"><b>Rp. 400K/Malam</b></h5>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 item gallery-image">
+                <div class="item-wrapper" data-toggle="modal" data-bs-toggle="modal" data-target="#t6Em4xNi0C-modal" data-bs-target="#t6Em4xNi0C-modal">
+                    <img class="w-100" src="/images/1-596x447.jpg" alt="Mobirise Website Builder" data-slide-to="1" data-bs-slide-to="1" data-target="#lb-t6Em4xNi0C" data-bs-target="#lb-t6Em4xNi0C">
+                    <div class="icon-wrapper">
+                        <span class="mobi-mbri mobi-mbri-search mbr-iconfont mbr-iconfont-btn"></span>
+                    </div>
+                </div>
+                <h6 class="mbr-item-subtitle mbr-fonts-style align-center mb-2 mt-2 display-7">Studio 2</h6>
+                <h5 class="mbr-item-subtitle mbr-fonts-style align-center mb-2 mt-2 display-7"><b>Rp. 525K/Malam</b></h5>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 item gallery-image">
+                <div class="item-wrapper" data-toggle="modal" data-bs-toggle="modal" data-target="#t6Em4xNi0C-modal" data-bs-target="#t6Em4xNi0C-modal">
+                    <img class="w-100" src="/images/4-596x447.jpg" alt="Mobirise Website Builder" data-slide-to="2" data-bs-slide-to="2" data-target="#lb-t6Em4xNi0C" data-bs-target="#lb-t6Em4xNi0C">
+                    <div class="icon-wrapper">
+                        <span class="mobi-mbri mobi-mbri-search mbr-iconfont mbr-iconfont-btn"></span>
+                    </div>
+                </div>
+                <h6 class="mbr-item-subtitle mbr-fonts-style align-center mb-2 mt-2 display-7">Suite</h6>
+                <h5 class="mbr-item-subtitle mbr-fonts-style align-center mb-2 mt-2 display-7"><b>Rp. 800K/Malam</b></h5>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 item gallery-image">
+                <div class="item-wrapper" data-toggle="modal" data-bs-toggle="modal" data-target="#t6Em4xNi0C-modal" data-bs-target="#t6Em4xNi0C-modal">
+                    <img class="w-100" src="/images/3-596x397.jpeg" alt="Mobirise Website Builder" data-slide-to="3" data-bs-slide-to="3" data-target="#lb-t6Em4xNi0C" data-bs-target="#lb-t6Em4xNi0C">
+                    <div class="icon-wrapper">
+                        <span class="mobi-mbri mobi-mbri-search mbr-iconfont mbr-iconfont-btn"></span>
+                    </div>
+                </div>
+                <h6 class="mbr-item-subtitle mbr-fonts-style align-center mb-2 mt-2 display-7">King Suite</h6>
+                <h5 class="mbr-item-subtitle mbr-fonts-style align-center mb-2 mt-2 display-7"><b>Rp. 1.1K/Malam</b></h5>
+            </div>
+        </div>
+
+        <div class="modal mbr-slider" tabindex="-1" role="dialog" aria-hidden="true" id="t6Em4xNi0C-modal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="carousel slide" id="lb-t6Em4xNi0C" data-interval="5000" data-bs-interval="5000">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="/images/2-596x447.jpg" alt="Mobirise Website Builder">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="/images/1-596x447.jpg" alt="Mobirise Website Builder">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="/images/4-596x447.jpg" alt="Mobirise Website Builder">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="/images/3-596x397.jpeg" alt="Mobirise Website Builder">
+                                </div>
+                            </div>
+                            <ol class="carousel-indicators">
+                                <li data-slide-to="0" data-bs-slide-to="0" class="active" data-target="#lb-t6Em4xNi0C" data-bs-target="#lb-t6Em4xNi0C"></li>
+                                <li data-slide-to="1" data-bs-slide-to="1" data-target="#lb-t6Em4xNi0C" data-bs-target="#lb-t6Em4xNi0C"></li>
+                                <li data-slide-to="2" data-bs-slide-to="2" data-target="#lb-t6Em4xNi0C" data-bs-target="#lb-t6Em4xNi0C"></li>
+                                <li data-slide-to="3" data-bs-slide-to="3" data-target="#lb-t6Em4xNi0C" data-bs-target="#lb-t6Em4xNi0C"></li>
+                            </ol>
+                            <a role="button" href="" class="close" data-dismiss="modal" data-bs-dismiss="modal" aria-label="Close">
+                            </a>
+                            <a class="carousel-control-prev carousel-control" role="button" data-slide="prev" data-bs-slide="prev" href="#lb-t6Em4xNi0C">
+                                <span class="mobi-mbri mobi-mbri-arrow-prev" aria-hidden="true"></span>
+                                <span class="sr-only visually-hidden">Previous</span>
+                            </a>
+                            <a class="carousel-control-next carousel-control" role="button" data-slide="next" data-bs-slide="next" href="#lb-t6Em4xNi0C">
+                                <span class="mobi-mbri mobi-mbri-arrow-next" aria-hidden="true"></span>
+                                <span class="sr-only visually-hidden">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="footer3 cid-s48P1Icc8J" once="footers" id="footer3-i">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+ 
+            </div>
+            <div class="row social-row">
+                <div class="social-list align-right pb-2">
+                    
+                    
+                    
+                    
+                    
+                    
+                <div class="soc-item">
+                        <a href="" target="_blank">
+                            <span class="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="" target="_blank">
+                            <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="" target="_blank">
+                            <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                        </a>
+                    </div></div>
+            </div>
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7"><br>Jl. Ranca Bentang 56-58 Ciumbuleuit, Bandung 40142, Indonesia
+                <br></p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="display-7" style="padding: 0;align-items: center;justify-content: center;flex-wrap: wrap;    align-content: center;display: flex;position: relative;height: 4rem;"><a href="https://mobiri.se/" style="flex: 1 1;height: 4rem;position: absolute;width: 100%;z-index: 1;"><img alt="" style="height: 4rem;" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="></a><p style="margin: 0;text-align: center;" class="display-7"> &#8204;</p><a style="z-index:1" href="https://mobirise.com/html-builder.html"></a></section><script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/ytplayer/index.js"></script>  <script src="assets/embla/embla.min.js"></script>  <script src="assets/embla/script.js"></script>  <script src="assets/dropdown/js/navbar-dropdown.js"></script>  <script src="assets/theme/js/script.js"></script>  
+  
+  
+</body>
+</html>
