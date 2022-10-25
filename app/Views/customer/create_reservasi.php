18096c0c02d4e52ca4d09f1ba314e24ad9e5cab1
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-10-25 18:02:13 +0700

    Reservasi

---
+++ app/Views/customer/create_reservasi.php
@@ -0,0 +1,130 @@
+<?php echo view('layout/header_konsumen'); ?>
+
+<div class="page-heading">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>Table</h3>
+                <p class="text-subtitle text-muted">Who does not love tables</p>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Table</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <!-- Here Code To Create Form -->
+    <section id="basic-horizontal-layouts">
+        <div class="row match-height">
+            <div class="col-md-6 col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Silahkan Pilih Pesanan Anda</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-body">
+                            <form action="/savereservasi/" method="POST" class="form form-horizontal">
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label>Name</label>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="form-group has-icon-left">
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="Name" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-person"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Mobile</label>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="form-group has-icon-left">
+                                                <div class="position-relative">
+                                                    <input type="number" class="form-control" placeholder="Mobile">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-phone"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Tipe Kamar</label>
+                                        </div>
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-group">
+                                                <select class="choices form-select">
+                                                    <option value="studio">Studio Rp 400000</option>
+                                                    <option value="studio II">Studio II Rp 500000</option>
+                                                    <option value="suite">Suite Rp 750000</option>
+                                                    <option value="kings suite">King Suite 1100000</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Tanggal Check In</label>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="form-group has-icon-left">
+                                                <div class="position-relative">
+                                                    <input type="date" class="form-control" placeholder="Tanggal Check In">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-phone"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Tanggal Check Out</label>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="form-group has-icon-left">
+                                                <div class="position-relative">
+                                                    <input type="date" class="form-control" placeholder="Tanggal Check In">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-phone"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Total Harga</label>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="form-group has-icon-left">
+                                                <div class="position-relative">
+                                                    <input type="number" class="form-control" placeholder="5000000" disabled>
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-phone"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 d-flex justify-content-end">
+                                            <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
+                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+</div>
+</section>
+</div>
+
+<?php echo view('layout/footer'); ?>
---
+++ app/Views/customer/reservasi.php
@@ -0,0 +1,104 @@
+<?php echo view('layout/header_konsumen'); ?>
+<div class="page-heading">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>Table</h3>
+                <p class="text-subtitle text-muted">Who does not love tables</p>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Table</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <section class="section">
+        <div class="row" id="table-head">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Table head options</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-body">
+                            <p>Berikut adalah Reservasi Kamar Hotel yang Anda Pesan</p>
+                            <button><a href="/createreservasi/">Buat Reservasi</a></button>
+                        </div>
+                        <!-- table head dark -->
+                        <div class="table-responsive">
+                            <table class="table mb-0">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>NAME</th>
+                                        <th>RATE</th>
+                                        <th>SKILL</th>
+                                        <th>TYPE</th>
+                                        <th>LOCATION</th>
+                                        <th>ACTION</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="text-bold-500">Michael Right</td>
+                                        <td>$15/hr</td>
+                                        <td class="text-bold-500">UI/UX</td>
+                                        <td>Remote</td>
+                                        <td>Austin,Taxes</td>
+                                        <td><a href="#"><i
+                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
+                                                    data-feather="mail"></i></a></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-bold-500">Morgan Vanblum</td>
+                                        <td>$13/hr</td>
+                                        <td class="text-bold-500">Graphic concepts</td>
+                                        <td>Remote</td>
+                                        <td>Shangai,China</td>
+                                        <td><a href="#"><i
+                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
+                                                    data-feather="mail"></i></a></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-bold-500">Tiffani Blogz</td>
+                                        <td>$15/hr</td>
+                                        <td class="text-bold-500">Animation</td>
+                                        <td>Remote</td>
+                                        <td>Austin,Texas</td>
+                                        <td><a href="#"><i
+                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
+                                                    data-feather="mail"></i></a></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-bold-500">Ashley Boul</td>
+                                        <td>$15/hr</td>
+                                        <td class="text-bold-500">Animation</td>
+                                        <td>Remote</td>
+                                        <td>Austin,Texas</td>
+                                        <td><a href="#"><i
+                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
+                                                    data-feather="mail"></i></a></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-bold-500">Mikkey Mice</td>
+                                        <td>$15/hr</td>
+                                        <td class="text-bold-500">Animation</td>
+                                        <td>Remote</td>
+                                        <td>Austin,Texas</td>
+                                        <td><a href="#"><i
+                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
+                                                    data-feather="mail"></i></a></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php echo view('layout/footer'); ?>
