daf6f8dd7b7e0eda29929957dbfb3fcbd62fc4b3
msultanr2057051019 <83525246+MSultan-2057051019@users.noreply.github.com>
2022-11-02 07:26:41 +0700

    read tabel customer dari db

--- app/Views/customer/reservasi.php
+++ app/Views/customer/reservasi.php
@@ -3,14 +3,13 @@
     <div class="page-title">
         <div class="row">
             <div class="col-12 col-md-6 order-md-1 order-last">
-                <h3>Table</h3>
-                <p class="text-subtitle text-muted">Who does not love tables</p>
+                <h3>Tabel Reservasi</h3>
             </div>
             <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Table</li>
+                        <li class="breadcrumb-item active" aria-current="page">Tabel Reservasi</li>
                     </ol>
                 </nav>
             </div>
@@ -33,65 +32,24 @@
                             <table class="table mb-0">
                                 <thead class="thead-dark">
                                     <tr>
-                                        <th>NAME</th>
-                                        <th>RATE</th>
-                                        <th>SKILL</th>
-                                        <th>TYPE</th>
-                                        <th>LOCATION</th>
-                                        <th>ACTION</th>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>Tipe</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    <?php $i = 1; ?>
+                                    <?php foreach ($customer as $k) : ?>
                                     <tr>
-                                        <td class="text-bold-500">Michael Right</td>
-                                        <td>$15/hr</td>
-                                        <td class="text-bold-500">UI/UX</td>
-                                        <td>Remote</td>
-                                        <td>Austin,Taxes</td>
-                                        <td><a href="#"><i
-                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
-                                                    data-feather="mail"></i></a></td>
+                                        <td><?= $i++; ?></td>
+                                        <td><?= $k['nama'] ?></td>
+                                        <td><?= $k['tipe'] ?></td>
+                                        <td><?= $k['status'] ?></td>
+                                        <td><a href="#"><i class="badge-circle badge-circle-light-secondary font-medium-1" data-feather="mail"></i></a></td>
                                     </tr>
-                                    <tr>
-                                        <td class="text-bold-500">Morgan Vanblum</td>
-                                        <td>$13/hr</td>
-                                        <td class="text-bold-500">Graphic concepts</td>
-                                        <td>Remote</td>
-                                        <td>Shangai,China</td>
-                                        <td><a href="#"><i
-                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
-                                                    data-feather="mail"></i></a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-bold-500">Tiffani Blogz</td>
-                                        <td>$15/hr</td>
-                                        <td class="text-bold-500">Animation</td>
-                                        <td>Remote</td>
-                                        <td>Austin,Texas</td>
-                                        <td><a href="#"><i
-                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
-                                                    data-feather="mail"></i></a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-bold-500">Ashley Boul</td>
-                                        <td>$15/hr</td>
-                                        <td class="text-bold-500">Animation</td>
-                                        <td>Remote</td>
-                                        <td>Austin,Texas</td>
-                                        <td><a href="#"><i
-                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
-                                                    data-feather="mail"></i></a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-bold-500">Mikkey Mice</td>
-                                        <td>$15/hr</td>
-                                        <td class="text-bold-500">Animation</td>
-                                        <td>Remote</td>
-                                        <td>Austin,Texas</td>
-                                        <td><a href="#"><i
-                                                    class="badge-circle badge-circle-light-secondary font-medium-1"
-                                                    data-feather="mail"></i></a></td>
-                                    </tr>
+                                    <?php endforeach; ?>
                                 </tbody>
                             </table>
                         </div>
